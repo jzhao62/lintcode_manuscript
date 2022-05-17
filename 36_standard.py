f2683fa89717297c72154873e331f0086adb9109
JINGYI ZHAO <zhao.elton@gmail.com>
2022-05-16 19:37:52 -0700

    Update 36_standard.py

--- 36_standard.py
+++ 36_standard.py
@@ -1,3 +1,5 @@
+
+# 翻转 m-> n之间的链表
 def reverse_between(self, head: ListNode, m: int, n: int) -> ListNode:
         dummy = ListNode(-1)
         dummy.next = head;
@@ -5,6 +7,9 @@ def reverse_between(self, head: ListNode, m: int, n: int) -> ListNode:
         for _ in range(m-1):
             p1 = p1.next;
             p2 = p2.next;
+                
+                
+# 找到第 m-1 个链表,
 
         prev1, prev2 = p1, p2
         p1=p1.next;
