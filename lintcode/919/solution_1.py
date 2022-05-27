52d91759d1b2bfda00bec93758c286984f6d80bc
jingyi <zhao.elton@gmail.com>
2022-05-27 15:54:50 -0700

    commit

---
+++ lintcode/919/solution_1.py
@@ -0,0 +1,29 @@
+from lintcode.intervals import Interval
+
+
+class Solution:
+    """
+    @param intervals: an array of meeting time intervals
+    @return: the minimum number of conference rooms required
+    """
+
+    def minMeetingRooms(self, intervals):
+        points = []
+        for interval in intervals:
+            points.append((interval.start, 1))
+            points.append((interval.end, -1))
+
+        meeting_rooms = 0
+        ongoing_meetings = 0
+        for _, delta in sorted(points):
+            ongoing_meetings += delta
+            meeting_rooms = max(meeting_rooms, ongoing_meetings)
+
+        return meeting_rooms
+
+
+intervals = [Interval(0, 30), Interval(5, 10), Interval(15, 20)]
+
+output = Solution().minMeetingRooms(intervals)
+
+print(output)
---
+++ lintcode/920/solution_1.py
@@ -0,0 +1,38 @@
+from typing import (
+    List,
+)
+from lintcode import (
+    Interval,
+)
+
+"""
+Definition of Interval:
+class Interval(object):
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+"""
+
+
+class Solution:
+    """
+    @param intervals: an array of meeting time intervals
+    @return: if a person could attend all meetings
+    """
+
+    def can_attend_meetings(self, intervals: List[Interval]) -> bool:
+
+        points = []
+
+        for interval in intervals:
+            points.append((interval.start, 1))
+            points.append((interval.end, -1))
+
+        max_meeting_rooms = 0
+        onging_meetings = 0
+
+        for _, delta in sorted(points):
+            onging_meetings += delta
+            max_meeting_rooms = max(max_meeting_rooms, onging_meetings)
+
+        return not points or max_meeting_rooms == 1
