5511b910ca1f7eb9be9b2c3d2be822e6f813e459
jingyi <zhao.elton@gmail.com>
2022-06-10 17:47:47 -0700

    fix draft for 1221

--- lintcode/1221/draft1.py
+++ lintcode/1221/draft1.py
@@ -1,10 +1,9 @@
-
-
 class TrieNode:
     def __init__(self):
         self.children = {}
         self.word = None;
         self.is_word = False;
+
 
 class Trie:
     def __init__(self):
@@ -15,14 +14,11 @@ class Trie:
 
         for c in word:
             if c not in node.children:
-                node.children[c] = TrieNode
+                node.children[c] = TrieNode()
             node = node.children[c]
 
         node.word = word
         node.is_word = True
-
-
-
 
 
 class Solution:
@@ -44,10 +40,6 @@ class Solution:
 
         return output
 
-
-
-
-
     def is_concatenated(self, node, word, idx):
         if idx == len(word):
             return node.is_word
@@ -56,13 +48,21 @@ class Solution:
             # 如果当前分支无关，跳过
             if word[idx] != c: continue;
 
+            # 如果当前node.children中包含了完整的 word，说明它肯定不是concatenated word
             if node.children[c].is_word and node.children[c].word == word: return False
 
-            if node.children[c].is_word and self.is_concatenated(self.root, word, idx+1): return True;
-            if self.is_concatenated(node.children[c], word, idx+1): return True;
+            # 如果 0-> idx 是word，并且idx+1 -> end 出现在其他分支中，并且是concatenated word，那么它的组合必然是concatenated word
 
+            if node.children[c].is_word and self.is_concatenated(self.root, word, idx + 1): return True;
+            # 如果 0-> idx 是word，并且idx+1 -> end 出现在接下来的分支，并且是concatenated word，那么它的组合必然是concatenated word
+
+            if self.is_concatenated(node.children[c], word, idx + 1): return True;
 
         return False;
 
 
+words = ["cat", "cats", "catsdogcats", "dog", "dogcatsdog", "hippopotamuses", "rat", "ratcatdogcat"]
 
+output = Solution().find_all_concatenated_words_in_a_dict(words)
+
+print(output)
