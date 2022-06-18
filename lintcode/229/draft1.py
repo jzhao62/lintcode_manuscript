8e417e9c8256d04ec869727b620d7d6aa2b4ac17
jingyi <zhao.elton@gmail.com>
2022-06-18 01:07:06 -0700

    fix for 229

--- lintcode/229/draft1.py
+++ lintcode/229/draft1.py
@@ -4,15 +4,24 @@ class Solution:
     @return: void
     """
     def stack_sorting(self, stk):
+        # 保证stk_2是单调递减
         stk_2 = []
         while stk:
+            # 首先分离stk顶的数字
             curr_top = stk.pop()
+
+            # 如果stk top > stk_2 top, 那么把 stk_2 top 拿回来
             while stk_2 and curr_top > stk_2[-1]:
                 stk.append(stk_2.pop())
 
+            # 上面那个while loop保证 stk_2 append的正确性
+
             stk_2.append(curr_top)
 
-        stk = stk_2[::-1]
+
+        # 区别在这里，不能new 一个 stk
+        while stk_2:
+            stk.append(stk_2.pop())
 
 
 
