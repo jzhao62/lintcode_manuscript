12dd892009d4e893c75e6ab88f7becb30e71b5a7
jingyi <zhao.elton@gmail.com>
2022-06-18 03:07:24 -0700

    init draft for 693

---
+++ lintcode/693/draft1.py
@@ -0,0 +1,29 @@
+class Solution:
+    def remove_kdigits(self, num: str, k: int) -> str:
+        stack = []
+
+        quota = k
+
+        for c in num:
+            while stack and int(c) < int(stack[-1]):
+                stack.pop()
+                quota -= 1
+
+            stack.append(c)
+
+        while quota > 0 and stack:
+            stack.pop()
+            quota -= 1
+
+        return "".join(stack)
+
+
+
+
+num = "1432219"
+k = 3
+
+
+x = Solution().remove_kdigits(num, k)
+
+print(x)
